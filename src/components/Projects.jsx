77dc0fb75a6e08d35d83ff2b5901032d8d884bcc
adrriiiz <talcopalanariz@gmail.com>
2026-05-17 16:53:35 +0200

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -28,7 +28,7 @@ export default function Projects() {
     <section id="projects" className="projects">
       <div className="container">
         <motion.h2 initial={{ opacity: 0, y: -20 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 0.6 }} viewport={{ once: true }}>
-          Proyectos
+          Projects
         </motion.h2>
         
         <motion.div 
